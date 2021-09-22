c48ae9999893c4e37ca8fbca0b1e887d8947cc3a
ThanuPriyaL <65939146+ThanuPriyaL@users.noreply.github.com>
2021-09-22 13:59:42 +0530

    Update Day2.sql

--- Day2.sql
+++ Day2.sql
@@ -4,13 +4,22 @@ select id from dept where dep_name='science'|| dep_name='maths';
 
 select * from stu where id in (select id from dept where dep_name='social'|| dep_name='maths');
 
+select * from stud where(marks>500 and namelike'%a')
+
+select * from dept d, stu s where s.marks>500 and d.dept_name='maths' and s.deptid=d.id;
+
+select * from stu s,(select * from dept where id>1)
+where s.marks>680 and d.id='science';
+
+select * from dept where id=(select deptid from stu where name='priya');
+
 select * from stu s, dept d where s.marks > 700 and d.dep_name= 'maths';
 
 delete from dept where dep_id=3;
 
 drop table dept;
 
-update student_details set marks=800 where id='1';
+update stu set marks=800 where id='1';
 
 select * from stu;
 
@@ -18,7 +27,7 @@ alter table stu add results varchar(10);
 
 alter table stu drop results;
 
-alter table stud modify results varchar(10);
+alter table stu modify results varchar(10);
 
 alter table stu modify results int;
 
